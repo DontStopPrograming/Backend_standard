d273c90f752bbc41b9e97ff01c457502e1d7d5a0
DontStopPrograming <01dyamond01@gmail.com>
2024-07-31 22:06:00 -0500

    Create: Initial Express Server Setup

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+
+import 'dotenv/config'
+
+
+const app = express()
+
+app.use(cors())
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
